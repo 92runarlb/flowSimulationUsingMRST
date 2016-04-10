a55c4587b56ed0cd1f5c0372b434c7f839dacfcd
Øystein S Klemetsdal <oysteskl@null05.math.ntnu.no>
2016-04-10 19:27:29 +0200

    Convergence test for VEM.

--- cases/checkVEM.m
+++ cases/checkVEM.m
@@ -7,18 +7,24 @@ addpath('../../vem/vem/mat/VEM2D/')
 run('../../project-mechanics-fractures/mystartup.m')
 
 f = @(X) zeros(size(X,1),1);
-C = -[.2,.2];
+C = -[.5,.5];
 gD = @(X) -log(1./(sqrt(sum(bsxfun(@minus, X, C).^2,2))));
 
+% f = @(X) sin(X(:,1));
+% gD = @(X) sin(X(:,1));
 
-nVec = [10,20,40,60];
+% f = @(X) 2 - 60*X(:,2);
+% gD = @(X) X(:,2)/45 - X(:,1).^2 + 10*X(:,2).^3;
+
+nVec = [20,40,80,160];
 nIt = numel(nVec);
 errVec = zeros(nIt, 3);
 
 for i = 1:nIt
     
     n = nVec(i);
-    G = cartGrid([n,n], [1,1]);
+%     G = cartGrid([n,n], [1,1]);
+    G = unitSquare(n,n);
     G = sortEdges(G);
     G = computeVEM2DGeometry(G);
 
@@ -26,21 +32,23 @@ for i = 1:nIt
 
     bc_VEM = VEM_addBC(G, [], boundaryEdges, 'pressure', gD);
 
-    sVEM1 = VEM2D_v3(G,0,1,bc_VEM);
-    sVEM2 = VEM2D_v3(G,0,2,bc_VEM);
+    sVEM1 = VEM2D_v3(G,f,1,bc_VEM);
+    sVEM2 = VEM2D_v3(G,f,2,bc_VEM);
 
 	u = [gD(G.nodes.coords); gD(G.faces.centroids); polygonInt_v2(G,1:G.cells.num,gD,7)./G.cells.volumes];
 
     err1 = sVEM1.nodeValues-u(1:G.nodes.num);
-    err2 = [sVEM2.nodeValues; sVEM2.edgeValues; sVEM2.cellMoments] - u;
+    err2 = [sVEM2.nodeValues; sVEM2.edgeValues] - u(1:(G.nodes.num + G.faces.num));
     
-    h = mean(G.cells.diameters); 
+    h = mean(G.cells.diameters);
+    area = sqrt(sum(G.cells.volumes.^2));
+    nK = G.cells.num;
     
-    errVec(i,:) = [h, h*norm(err1, 'inf'), h*norm(err2, 'inf')];
+    errVec(i,:) = [h, l2Norm(G,err1,1), l2Norm(G,err2,2)];
     
 end
 
-loglog(errVec(:,1), errVec(:,2), errVec(:,2), errVec(:,3));
+loglog(errVec(:,1), errVec(:,2), errVec(:,1), errVec(:,3));
 legend('VEM 1st order', 'VEM 2nd order');
 p1 = polyfit(log(errVec(:,1)), log(errVec(:,2)),1);
 p2 = polyfit(log(errVec(:,1)), log(errVec(:,3)),1);
